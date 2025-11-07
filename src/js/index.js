a8c7af8b57e5447d40683fdb50b747f025a44450
gelinruozhi <gelinruozhi@gmail.com>
2025-11-07 17:26:46 +0800

    a

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,48 @@
 
-let firstSlideContainer = 
-document.getElementsByClassName("slide--content")[0];
+// 获取DOM元素并添加错误处理
+function getElement(selector, index = 0) {
+  const elements = document.querySelectorAll(selector);
+  return elements.length > index ? elements[index] : null;
+}
 
-let secondSlideContainer = 
-document.getElementsByClassName("slide--content--one")[0];
+function setElementStyle(element, style) {
+  if (element) {
+    element.style.cssText = style;
+  }
+}
 
-let secondCanvas = 
-document.getElementsByClassName("second--canvas")[0];
+let firstSlideContainer = getElement('.slide--content');
+let secondSlideContainer = getElement('.slide--content--one');
+let secondCanvas = getElement('.second--canvas');
+let thirdCanvas = getElement('.third--canvas');
+let firstSlide = getElement('.first--slide');
 
-secondSlideContainer.setAttribute("style","display:none");
-secondCanvas.setAttribute("style","display:none");
+// 初始隐藏
+setElementStyle(secondSlideContainer, 'display: none');
+setElementStyle(secondCanvas, 'display: none');
+setElementStyle(thirdCanvas, 'display: none');
 
-let thirdCanvas = 
-document.getElementsByClassName("third--canvas")[0];
-
-thirdCanvas.setAttribute("style","display:none");
-
-
-
+// 第一个切换
 let containerToggleOne = setTimeout(function(){
-	firstSlideContainer.setAttribute("style","display:none");
-	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+  setElementStyle(firstSlideContainer, 'display: none');
+  setElementStyle(secondSlideContainer, 'display: block');
+}, 2500);
 
-
+// 移除第一个页面
 let removeFirstSlide = setTimeout(function(){
-	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
-	secondCanvas.setAttribute("style","display:block");
-},6500);
+  setElementStyle(firstSlide, 'display: none');
+  setElementStyle(secondCanvas, 'display: block');
+}, 6500);
 
+// 移除第二个页面
 let removeSecondCanvas = setTimeout(function(){
-	secondCanvas.setAttribute("style","display:none");
-	thirdCanvas.setAttribute("style","display:block")
-},9800)
+  setElementStyle(secondCanvas, 'display: none');
+  setElementStyle(thirdCanvas, 'display: block');
+}, 9800);
+
+// 添加清理函数，防止内存泄漏
+window.addEventListener('unload', function() {
+  clearTimeout(containerToggleOne);
+  clearTimeout(removeFirstSlide);
+  clearTimeout(removeSecondCanvas);
+});
